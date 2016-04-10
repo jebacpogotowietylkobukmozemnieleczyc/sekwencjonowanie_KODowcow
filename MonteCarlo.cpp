117353086ef5725bc1dde5e91a0b2556a103cd72
Your Name <you@example.com>
2016-04-10 19:31:48 +0200

    Z Fartem dla prawdziwych a lamusy dupa cicho.

--- MonteCarlo.cpp
+++ MonteCarlo.cpp
@@ -5,20 +5,21 @@
 
 
 int MonteCarlo::successor(uint32_t nucleotide, unsigned offset) {
-    tuple<uint32_t,unsigned> maxRate {0,0};
-    vector<uint32_t > successors;
+    tuple<uint32_t, unsigned> maxRate{0, 0};
+    vector<uint32_t> successors;
     int base = (nucleotide << (2 * offset)) & ((1 << 20) - 1);
-    for (int j = 0; j <  pow(4,offset); ++j) {
-        if(microArray[j]) {
+    for (int j = 0; j < pow(4, offset); ++j) {
+        if (microArray[j]) {
             successors.push_back(j);
         }
     }
-    if(successors.size()>2){
-        for(uint32_t element :successors){
-            tuple<uint32_t,unsigned> newRate {element,runForward(element)};
-            maxRate = max(maxRate,newRate,[](const tuple<uint32_t ,unsigned >& tuple1,const tuple<uint32_t ,unsigned >& tuple2){
-                return get<1>(tuple1) < get<1>(tuple2);
-            });
+    if (successors.size() > 2) {
+        for (uint32_t element :successors) {
+            tuple<uint32_t, unsigned> newRate{element, runForward(element)};
+            maxRate = max(maxRate, newRate,
+                          [](const tuple<uint32_t, unsigned> &tuple1, const tuple<uint32_t, unsigned> &tuple2) {
+                              return get<1>(tuple1) < get<1>(tuple2);
+                          });
         }
         result.push_back(get<0>(maxRate));
         microArray[get<0>(maxRate)] = false;
@@ -33,24 +34,25 @@ int MonteCarlo::successor(uint32_t nucleotide, unsigned offset) {
     return -1;
 }
 
-int MonteCarlo::predecessor(uint32_t nucleotide, unsigned offset){
-    tuple<uint32_t,unsigned> maxRate {0,0};
-    vector<uint32_t > predecessors;
+int MonteCarlo::predecessor(uint32_t nucleotide, unsigned offset) {
+    tuple<uint32_t, unsigned> maxRate{0, 0};
+    vector<uint32_t> predecessors;
 
-    int step = 262144/pow(4,offset-1);
-    int base = (nucleotide>>(2*offset)) & ((1<<20)-1);
-    for (int j = base; j <  MATRIX_COUNT; j+=step) {
-        if(microArray[j]) {
+    int step = 262144 / pow(4, offset - 1);
+    int base = (nucleotide >> (2 * offset)) & ((1 << 20) - 1);
+    for (int j = base; j < MATRIX_COUNT; j += step) {
+        if (microArray[j]) {
             predecessors.push_back(j);
         }
     }
 
-    if(predecessors.size() > 2){
-        for(uint32_t element :predecessors){
-            tuple<uint32_t,unsigned> newRate {element,runForward(element)};
-            maxRate = max(maxRate,newRate,[](const tuple<uint32_t ,unsigned >& tuple1,const tuple<uint32_t ,unsigned >& tuple2){
-                return get<1>(tuple1) < get<1>(tuple2);
-            });
+    if (predecessors.size() > 2) {
+        for (uint32_t element :predecessors) {
+            tuple<uint32_t, unsigned> newRate{element, runForward(element)};
+            maxRate = max(maxRate, newRate,
+                          [](const tuple<uint32_t, unsigned> &tuple1, const tuple<uint32_t, unsigned> &tuple2) {
+                              return get<1>(tuple1) < get<1>(tuple2);
+                          });
         }
         result.push_front(get<0>(maxRate));
         microArray[get<0>(maxRate)] = false;
@@ -70,34 +72,35 @@ int MonteCarlo::recursiveSuccessor(uint32_t nucleotide, unsigned maxOffset) {
     int cRate = 0;
     int step = 262144;
     for (int i = 1; i <= maxOffset; ++i) {
-        int base = (nucleotide>>(2*i)) & ((1<<20)-1);
+        int base = (nucleotide >> (2 * i)) & ((1 << 20) - 1);
         for (int j = 0; j < pow(4, i); ++j) {
             int r = base + randomVectors[i - 1].at(j) * step;
             if (microArray[r]) {
                 result.push_front(r);
                 microArray[r] = false;
-                negativeError+=i-1;
+                negativeError += i - 1;
                 if ((++countOffset[i - 1]) == randomVectorLimit[i - 1]) {
                     shuffleVector(i);
-                    randomVectorIterators[i-1] = randomVectorIterators[i-1] >= randomVector.size() ? 0 : randomVectorIterators[i-1];
-                    randomVectorLimit[i - 1] += randomVector[randomVectorIterators[i-1]++];
+                    randomVectorIterators[i - 1] =
+                            randomVectorIterators[i - 1] >= randomVector.size() ? 0 : randomVectorIterators[i - 1];
+                    randomVectorLimit[i - 1] += randomVector[randomVectorIterators[i - 1]++];
                 }
                 cRate = recursiveSuccessor(r, maxOffset);
                 --countOffset[i - 1];
-                negativeError-=i-1;
+                negativeError -= i - 1;
                 microArray[r] = true;
                 result.pop_front();
                 return cRate;
             }
         }
-        step/=4;
+        step /= 4;
     }
 //    printResultAsString();
 //    printStats();
     return rate();
 }
 
-int MonteCarlo::recursivePredecessor(uint32_t nucleotide, unsigned maxOffset){
+int MonteCarlo::recursivePredecessor(uint32_t nucleotide, unsigned maxOffset) {
     int cRate = 0;
     for (int i = 1; i <= maxOffset; ++i) {
         int base = (nucleotide << (2 * i)) & ((1 << 20) - 1);
@@ -106,15 +109,16 @@ int MonteCarlo::recursivePredecessor(uint32_t nucleotide, unsigned maxOffset){
             if (microArray[r]) {
                 result.push_back(r);
                 microArray[r] = false;
-                negativeError+=i-1;
+                negativeError += i - 1;
                 if ((++countOffset[i - 1]) == randomVectorLimit[i - 1]) {
                     shuffleVector(i);
-                    randomVectorIterators[i-1] = randomVectorIterators[i-1] >= randomVector.size() ? 0 : randomVectorIterators[i-1];
-                    randomVectorLimit[i - 1] += randomVector[randomVectorIterators[i-1]++];
+                    randomVectorIterators[i - 1] =
+                            randomVectorIterators[i - 1] >= randomVector.size() ? 0 : randomVectorIterators[i - 1];
+                    randomVectorLimit[i - 1] += randomVector[randomVectorIterators[i - 1]++];
                 }
                 cRate = recursiveSuccessor(r, maxOffset);
                 --countOffset[i - 1];
-                negativeError-=i-1;
+                negativeError -= i - 1;
                 microArray[r] = true;
                 result.pop_back();
                 return cRate;
@@ -128,12 +132,16 @@ int MonteCarlo::recursivePredecessor(uint32_t nucleotide, unsigned maxOffset){
 
 unsigned MonteCarlo::runForward(uint32_t nucleotide) {
     //todo //musi być mniejszy niż MAX_NEGATIVE
-    return runN(nucleotide, 2,50,[this](uint32_t nucleotide, unsigned offset){ return recursiveSuccessor(nucleotide,offset);}); //musi być mniejszy niż MAX_NEGATIVE
+    return runN(nucleotide, 2, 50, [this](uint32_t nucleotide, unsigned offset) {
+        return recursiveSuccessor(nucleotide, offset);
+    }); //musi być mniejszy niż MAX_NEGATIVE
 
 }
 
-unsigned MonteCarlo::runBackward(uint32_t nucleotide){
+unsigned MonteCarlo::runBackward(uint32_t nucleotide) {
     //todo //musi być mniejszy niż MAX_NEGATIVE
-    return runN(nucleotide, 2,50,[this](uint32_t nucleotide, unsigned offset){ return recursivePredecessor(nucleotide,offset);}); //musi być mniejszy niż MAX_NEGATIVE
+    return runN(nucleotide, 2, 50, [this](uint32_t nucleotide, unsigned offset) {
+        return recursivePredecessor(nucleotide, offset);
+    }); //musi być mniejszy niż MAX_NEGATIVE
 
 }
